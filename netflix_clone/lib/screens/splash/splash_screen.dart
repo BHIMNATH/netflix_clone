4985e7ac36cd53330ed89a4a2c66a9c8cb4f6098
Aswin Bhim Nath <aswismail@gmail.com>
2026-09-15 04:16:02 +0530

    Completed profile, downloads and more screens

---
+++ netflix_clone/lib/screens/splash/splash_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screens/profiles/profile_selection_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      if (!mounted) return;
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ProfileSelectionScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+          'assets/logos_netflix.png',
+          width: 180,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
