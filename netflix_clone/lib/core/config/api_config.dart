b4bd27b811af4ca6ae761e113695e7ff8364ef45
Aswin Bhim Nath <aswismail@gmail.com>
2026-09-15 13:13:55 +0530

    Update api_config.dart

--- netflix_clone/lib/core/config/api_config.dart
+++ netflix_clone/lib/core/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String apiKey = '173503fa41739829c79173dedaa47e14';
+  static const String apiKey = 'TMDB_API_KEY';
   static const String baseUrl = 'https://api.themoviedb.org/3';
 
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
